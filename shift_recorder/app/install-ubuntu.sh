bbfb9dd8bdd5b6febaf642ff38e78d7be0afaf4d
safalDeakin <sasstha@Safals-MacBook-Pro.local>
2024-03-03 08:31:47 +0545

    update Dockerfile for shift_rcorder

--- shift_recorder/app/install-ubuntu.sh
+++ shift_recorder/app/install-ubuntu.sh
@@ -1,3 +1,4 @@
-apt-get update
-apt-get install -y python3-pip
+apt update
+#export DEBIAN_FRONTEND=noninteractive
+apt install -y python3-pip
 pip3 install --no-cache-dir --upgrade -r requirements.txt
